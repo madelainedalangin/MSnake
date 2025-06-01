810117a3723ff24747b4e13e4744ead765e3adae
Madelaine Dalangin <madelainedalangin@gmail.com>
2025-06-01 15:46:04 -0600

    Initial Commit

---
+++ MSnake/MSnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSnakeApp.swift
+//  MSnake
+//
+//  Created by Madelaine Dalangin on 2025-06-01.
+//
+
+import SwiftUI
+
+@main
+struct MSnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MSnakeTests/MSnakeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MSnakeTests.swift
+//  MSnakeTests
+//
+//  Created by Madelaine Dalangin on 2025-06-01.
+//
+
+import Testing
+@testable import MSnake
+
+struct MSnakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
